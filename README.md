68c9f5198f12909eec6ece555d68702f1e610a9f
Jibec <jean-baptiste@holcroft.fr>
2017-02-21 13:42:03 +0100

    Fully working content translation

--- README.md
+++ README.md
@@ -2,12 +2,32 @@ YunoHost Documentation - POC Pelican + po4a
 
 # Dependences
 
+## Pelican (python)
+
 Install Pelican (I use version 3.6.3 on Fedora).
 
+## Po4a
+
+Go to po4a-keyvalue_text and run:
+
+> perl Build.PL
+> ./Build
+
+Po4a will ask you to install needed packages.
+It's a clone of Brian Excelbierd's repository https://github.com/bexelbie/po4a/tree/keyvalue_text
+
 # Test it
+
+## Build translation
+
+Run the build script (it will go to Makefile someday):
+
+> ./build-translation
+
+## Generate content
 
 Run the development server:
 
 > ./develop_server.sh start 8080
 
-Then go on firefox to see the static website
+Then go on firefox to see the static website with http://localhost:8080
--- build-translation.sh
+++ build-translation.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-BASEDIR=$(pwd)
+BASEDIR=.
 DOCDIR=$BASEDIR/content-en/
 OUTPUTDIR=$BASEDIR/content/
+PO4ACONF=./po4a.cfg
 
 # clean all
 rm -r $OUTPUTDIR*
@@ -10,10 +11,16 @@ rm -r $OUTPUTDIR*
 # copy file structure and english content
 cp -r $DOCDIR* $OUTPUTDIR
 
+find $DOCDIR -name '*.md' | sed -r 's!(.*)(content)-en(.*)\.md![type: text] \1\2-en\3.md $lang:\1\2\3.$lang.md!' >> $PO4ACONF
+
+awk '!x[$0]++' $PO4ACONF > $PO4ACONF.tmp
+
+cp $PO4ACONF.tmp $PO4ACONF
+rm $PO4ACONF.tmp
+
 # generate po files and generate translated content
-PERLLIB=./po4a-keyvalue_text/lib ./po4a-keyvalue_text/po4a ./po4a.cfg \
+PERLLIB=./po4a-keyvalue_text/lib ./po4a-keyvalue_text/po4a $PO4ACONF \
 	--package-name "YunoHost-Documentation" \
 	--package-version `date +%Y%m%d`\
 	--msgid-bugs-address contrib@list.yunohost.org \
-	--master-charset utf8 \
-	--debug
+	--master-charset utf8
